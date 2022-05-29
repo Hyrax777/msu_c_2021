314f94748674197e0a946adfda7ee81b2a62bbe4
Hyrax777 <43938157+Hyrax777@users.noreply.github.com>
2022-05-29 21:50:30 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int proc(void)
+{
+    int pid = fork();
+    if (!pid) {
+        write(1, "1\n", 2);
+    }
+    return pid;
+}
+
+
+int main() {
+    proc(), proc(), proc();
+    return 0;
+}
